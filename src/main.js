ba6339fcd289f01b1f4b7e3fce732d597b6637e5
bohemian-ctrl <1582842406@qq.com>
2026-01-21 17:33:38 +0800

    deploy

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 
 Vue.config.productionTip = false
 // window.CESIUM_BASE_URL = '/zunyi_nishiliu/dist/cesium/'
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiOTA0NzFmOC1kYmNmLTQzMDctYjQxYS1iOTIwOWZhMTBmODkiLCJpZCI6Mzc1MTkwLCJpYXQiOjE3NjgyOTU3OTZ9.LSMLo60NHgkaMtgmxWSTGt_XYLpAp01StwJwMaiL8p4";
+    
 window.CESIUM_BASE_URL = 'https://cdn.jsdelivr.net/npm/cesium@1.107/Build/Cesium/'
 new Vue({
   router,
